7ffea476f753499fb637d6c550e03f624230b042
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-17 12:02:34 +0000

    [課題チェックシート]18列目

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit, :new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
