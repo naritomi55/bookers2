f90e37a1995471f84430aee30335550aeef500e5
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-19 22:58:45 +0000

    [課題チェックシート]26列目

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
     user_path(current_user.id)
   end
 
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
   private
 
     def configure_permitted_parameters
