54f5b7d73448fb62a846c3f083dcd3dc1056d2a4
KeiichiNaritomi <104675165+naritomi55@users.noreply.github.com>
2022-05-21 09:44:29 +0000

    [課題チェックシート]28行目

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to user_path(current_user)
+    redirect_to book_path(@book.id)
   end
 
   private
